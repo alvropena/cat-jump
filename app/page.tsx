98ce0536a183c2b91e0f6d1e4e374d54de798792
alvropena <me@alvropena.com>
2024-04-01 15:30:55 -0700

    Playing

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>
-      <p>Ange is the cutest</p>
+    <div className="h-screen flex flex-col justify-center items-center">
+      <p className="text-6xl text-center">Ange is the cutest forever</p>
+      <div className="flex items-center justify-center">
+        <p className='text-6xl'>🇵🇪</p>
+        <p className='text-6xl'>🇮🇹</p>
+        <p className='text-6xl'>❤️</p>
+      </div>
     </div>
   )
 }
