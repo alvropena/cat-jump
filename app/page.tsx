532d0a938e17fc5693542d10eea6afbe6a2a000b
alvropena <me@alvropena.com>
2024-04-01 16:04:49 -0700

    Updating style

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Page() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <p className="text-6xl text-center">Ange is the cutest forever</p>
+      <p className="text-4xl text-center">Ange is the cutest</p>
       <div className="flex items-center justify-center">
-        <p className='text-6xl'>🇵🇪</p>
-        <p className='text-6xl'>🇮🇹</p>
-        <p className='text-6xl'>❤️</p>
+        <p className='text-3xl'>🇵🇪</p>
+        <p className='text-3xl'>🇮🇹</p>
+        <p className='text-3xl'>❤️</p>
       </div>
     </div>
   )
