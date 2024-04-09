d3901dcba516e3b8d40059259c0c65030d37454a
alvropena <me@alvropena.com>
2024-04-09 09:49:17 -0700

    Incorporated Dark Mode theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 "use client"
 
+import * as React from "react"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { useTheme } from "next-themes"
+
 import { useState, useEffect } from "react";
 import Image from "next/image";
 
@@ -8,7 +12,39 @@ import { CarouselItem, CarouselContent, CarouselPrevious, CarouselNext, Carousel
 import { Dialog, DialogTrigger, DialogContent, DialogTitle, DialogHeader, DialogFooter, DialogDescription } from "@/components/ui/dialog"
 import { Button } from "@/components/ui/button"
 import { Input } from '@/components/ui/input'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
+function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 
 export default function Component() {
@@ -30,9 +66,9 @@ export default function Component() {
   }, []);
 
   return (
-    <div className="flex flex-col h-screen w-full items-center justify-center p-4 bg-black">
+    <div className="flex flex-col h-screen w-full items-center justify-center p-4">
       <header className="w-full text-center p-4">
-
+        <ModeToggle />
       </header>
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <DialogContent>
